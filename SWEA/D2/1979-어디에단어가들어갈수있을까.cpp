de71a561cc64ba2fd1063f26d6c0ffc518715c97
yamkim <coramdeo0708@gmail.com>
2020-09-19 12:13:51 +0900

    [dp] upload NUMB3RS solution and description

--- SWEA/D2/1979-어디에단어가들어갈수있을까.cpp
+++ SWEA/D2/1979-어디에단어가들어갈수있을까.cpp
@@ -10,13 +10,21 @@ int board[17][17];
 int N, K;
 
 bool rowCheck(int y, int x, int k) {
-	if (!(board[y][x] == 0 && board[y][x + k] == 0))
-		return (false);
-	for (int i = 0; i < k; ++i) {
-		if (x + 1 + i < N || !board[y][x + i + 1])
-			return (false);
-	}
-	return (true);
+	if (x > N) return (false);
+	if (k == K) return (board[y][x + 1] == 0 ? true : false);
+	if (board[y][x + 1] == 1)
+		return(rowCheck(y, x + 1, k + 1));
+	else
+		return(false);
+}
+
+bool colCheck(int y, int x, int k) {
+	if (y > N) return (false);
+	if (k == K) return (board[y + 1][x] == 0 ? true : false);
+	if (board[y + 1][x] == 1)
+		return(colCheck(y + 1, x, k + 1));
+	else
+		return(false);
 }
 
 int main(void) {
@@ -32,11 +40,16 @@ int main(void) {
 			for (int j = 1; j <= N; ++j)
 				cin >> board[i][j];
 		int res = 0;
-		for (int i = 0; i < N + 2 - K; ++i) {
-			for (int j = 0; j < N + 2 - K; ++j) {
-				if(rowCheck(i, j, K))
-					++res;
+		for (int i = 0; i <= N; ++i) {
+			for (int j = 0; j < N + 1 - K; ++j) {
+				if (board[i][j] == 0)
+					if (rowCheck(i, j, 0))
+						++res;
+				if (board[j][i] == 0)
+					if (colCheck(j, i, 0))
+						++res;
 			}
+	
 		}
 		printf("#%d %d\n", t, res);
 	}
